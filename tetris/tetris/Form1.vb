53429e48ca608101b2b85cb1e48e7e380e4e8048
wynner0 <wynn.john.george@gmail.com>
2014-04-21 20:38:16 +0200

    apr 21

--- tetris/tetris/Form1.vb
+++ tetris/tetris/Form1.vb
@@ -1,9 +1,20 @@
-﻿Public Class Form1
+﻿Public Class alakzat
+    Dim alak As Integer
+    Dim posy As Integer
+    Dim posx As Integer
+    Dim negyzetek(0 To 3) As Point
+    Public alakzat(
+
+End Class
+Public Class Form1
     Dim g As Graphics
     Dim mozgo(9, 15) As Boolean
     Dim allo(9, 15) As Boolean
     Dim alak As Integer
-    Dim pos As Integer = 0
+    Dim posy As Integer = 15
+    Dim posx As Integer
+    Dim a As alakzat
+    Dim szelesseg As Integer = 0
 
     Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
 
@@ -13,33 +24,20 @@
 
         For x = 0 To 9
             For y = 0 To 15
-                
+
                 If allo(x, y) Then
                     g.DrawRectangle(Pens.Pink, (x) * 17, (y) * 17, 15, 15)
                 End If
             Next
         Next
     End Sub
-    Private Sub mozgorajzol()
+    Private Sub negyzetrajzol(x As Integer, y As Integer)
         g = Panel1.CreateGraphics
-
-        For x = 0 To 9
-            For y = 0 To 15
-                If mozgo(x, y) Then
-                    g.DrawRectangle(Pens.Black, (x) * 17, (y) * 17, 15, 15)
-                Else
-                    g.DrawRectangle(Pens.White, (x) * 17, (y) * 17, 15, 15)
-                End If
-               
-            Next
-        Next
-
-
+        g.DrawRectangle(Pens.Black, (x) * 17, (y) * 17, 15, 15)
     End Sub
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
 
 
-        mozgorajzol()
         allorajzol()
         eses()
         érkezés()
@@ -82,18 +80,18 @@
             For y = 15 To 0 Step -1
 
                 If mozgo(x, y) And y < 15 Then
-                    'g.DrawRectangle(Pens.Black, (x) * 17, (y + 1) * 17, 15, 15)
-                    'g.DrawRectangle(Pens.White, (x) * 17, (y) * 17, 15, 15)
+                    g.DrawRectangle(Pens.Black, (x) * 17, (y + 1) * 17, 15, 15)
+                    g.DrawRectangle(Pens.White, (x) * 17, (y) * 17, 15, 15)
                     mozgo(x, y) = False
                     mozgo(x, y + 1) = True
-                    pos = y + 1
+                    posy += 1
                 End If
             Next
         Next
 
     End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        g = Panel1.CreateGraphics
+
 
         For Each a In mozgo
             a = False
@@ -104,13 +102,7 @@
 
 
 
-        For x = 0 To 9
-            For y = 0 To 15
-                If mozgo(x, y) = True Then
-                    g.DrawRectangle(Pens.Black, x * 16, y * 16, 15, 15)
-                End If
-            Next
-        Next
+
 
 
     End Sub
@@ -119,32 +111,40 @@
         Timer1.Start()
         Dim r As New Random()
         alak = r.Next(1, 4)
-
+        posy = 0
         If alak = 1 Then
-
+            szelesseg = 1
             mozgo(3, 0) = True
             mozgo(4, 0) = True
             mozgo(5, 0) = True
             mozgo(6, 0) = True
+            negyzetrajzol(3, 0)
+            negyzetrajzol(4, 0)
+            negyzetrajzol(5, 0)
+            negyzetrajzol(6, 0)
+
         End If
         If alak = 2 Then
-
+            szelesseg = 2
             mozgo(4, 0) = True
             mozgo(5, 0) = True
-            If r.Next(1, 3) = 1 Then
-                mozgo(4, 1) = True
-                mozgo(4, 2) = True
-
-            Else
-
-                mozgo(5, 2) = True
-                mozgo(5, 1) = True
-
-            End If
-
+            mozgo(4, 1) = True
+            mozgo(4, 2) = True
+            negyzetrajzol(4, 0)
+            negyzetrajzol(5, 0)
+            negyzetrajzol(4, 1)
+            negyzetrajzol(4, 2)
         End If
         If alak = 3 Then
-
+            szelesseg = 2
+            mozgo(4, 0) = True
+            mozgo(5, 0) = True
+            mozgo(5, 2) = True
+            mozgo(5, 1) = True
+            negyzetrajzol(4, 0)
+            negyzetrajzol(5, 0)
+            negyzetrajzol(5, 2)
+            negyzetrajzol(5, 1)
         End If
     End Sub
 
@@ -154,11 +154,12 @@
             For x = 0 To 9
                 For y = 15 To 0 Step -1
 
-                    If mozgo(x, y) And x > 0 Then
+                    If mozgo(x, y) And x > 0 + szelesseg - 1 Then
 
                         mozgo(x - 1, y) = True
                         mozgo(x, y) = False
-                        mozgorajzol()
+                        g.DrawRectangle(Pens.Black, (x - 1) * 17, (y) * 17, 15, 15)
+                        g.DrawRectangle(Pens.White, (x) * 17, (y) * 17, 15, 15)
                     End If
                 Next
             Next
@@ -167,13 +168,16 @@
 
 
         If e.KeyChar = Convert.ToChar(Keys.D.ToString.ToLower) Then
+
             For x = 9 To 0 Step -1
                 For y = 15 To 0 Step -1
 
-                    If mozgo(x, y) And x < 9 Then
+                    If mozgo(x, y) And x < 9 - szelesseg + 1 Then
                         mozgo(x, y) = False
                         mozgo(x + 1, y) = True
-                        mozgorajzol()
+                        g.DrawRectangle(Pens.Black, (x + 1) * 17, (y) * 17, 15, 15)
+                        g.DrawRectangle(Pens.White, (x) * 17, (y) * 17, 15, 15)
+
                     End If
                 Next
             Next
@@ -182,6 +186,6 @@
     End Sub
 
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
-       
+
     End Sub
 End Class
